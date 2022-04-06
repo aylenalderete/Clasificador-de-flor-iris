d6ea1c5d3fe823aba6b0b82e006882d46d5a2835
aylenalderete <aylenalderete@hotmail.com>
2022-04-05 22:36:29 -0300

    cambios

--- Irisflower_project/utils.py
+++ Irisflower_project/utils.py
@@ -5,8 +5,7 @@ import pickle
 
 def preprocessdata(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm):
     test_data = [[SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm] ]
-    print(test_data, 'hola')
-    trained_model = pickle.loads('modelo_entrenado.pkl')
+    trained_model = joblib.load('modelo_entrenado.pkl')
     prediction = trained_model.predict(test_data) 
 
     return prediction 
